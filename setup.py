71500ce09d638505c14a93194fedc5bade32c6e6
Karl Kroening <karlk@kralnet.us>
2017-05-27 18:01:52 -1000

    Add keywords in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,47 @@
+from ffmpeg._filters import __all__ as filter_names
 from distutils.core import setup
+
+
+file_formats = [
+    'aac',
+    'ac3',
+    'avi',
+    'bmp'
+    'flac',
+    'gif',
+    'mov',
+    'mp3',
+    'mp4',
+    'png',
+    'raw',
+    'rawvideo',
+    'wav',
+]
+file_formats += ['.{}'.format(x) for x in file_formats]
+
+misc_keywords = [
+    '-vf',
+    'a/v',
+    'audio',
+    'dsp',
+    'FFmpeg',
+    'ffmpeg',
+    'ffprobe',
+    'filtering',
+    'filter_complex',
+    'movie',
+    'render',
+    'signals',
+    'sound',
+    'streaming',
+    'streams',
+    'vf',
+    'video',
+    'wrapper',
+]
+
+keywords = misc_keywords + file_formats + filter_names
+
 setup(
     name = 'ffmpeg-python',
     packages = ['ffmpeg'],
@@ -7,21 +50,7 @@ setup(
     author = 'Karl Kroening',
     author_email = 'karlk@kralnet.us',
     url = 'https://github.com/kkroening/ffmpeg-python',
-    download_url = 'https://github.com/kkroening/ffmpeg-python/archive/0.1.tar.gz',
-    keywords = [
-        'a/v',
-        'audio',
-        'dsp',
-        'FFmpeg',
-        'ffmpeg',
-        'ffprobe',
-        'filtering',
-        'render',
-        'signals',
-        'streaming',
-        'streams',
-        'video',
-        'wrapper',
-    ],
+    download_url = 'https://github.com/kkroening/ffmpeg-python/archive/0.1.1.tar.gz',
     classifiers = [],
+    keywords = keywords,
 )
