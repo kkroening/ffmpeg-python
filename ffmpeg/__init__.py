0b1259238f745b25ad1c22d9107a2ba5dd16144f
Karl Kroening <karlk@kralnet.us>
2017-07-06 03:40:56 -0600

    #18: re-add __init__.py changes

--- ffmpeg/__init__.py
+++ ffmpeg/__init__.py
@@ -4,5 +4,6 @@ from . import _filters, _ffmpeg, _run
 from ._filters import *
 from ._ffmpeg import *
 from ._run import *
+from ._view import *
 
-__all__ = _filters.__all__ + _ffmpeg.__all__ + _run.__all__
+__all__ = _filters.__all__ + _ffmpeg.__all__ + _run.__all__ + _view.__all__
