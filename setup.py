562be84048c3939128b2614d2c7abe16c8913478
Karl Kroening <karlk@kralnet.us>
2017-11-20 00:34:17 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from textwrap import dedent
 import subprocess
 
-version = '0.1.6'
+version = '0.1.8'
 download_url = 'https://github.com/kkroening/ffmpeg-python/archive/v{}.zip'.format(version)
 
 long_description = dedent("""\
