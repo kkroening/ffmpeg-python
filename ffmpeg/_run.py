c21e8c103ffb535f4e1a9702d1b2881d1136efd6
Karl Kroening <karlk@kralnet.us>
2018-06-02 00:25:47 -0700

    Support video_size output tuple

--- ffmpeg/_run.py
+++ ffmpeg/_run.py
@@ -1,10 +1,9 @@
 from __future__ import unicode_literals
-
 from .dag import get_outgoing_edges, topo_sort
 from ._utils import basestring
 from builtins import str
 from functools import reduce
-from past.builtins import basestring
+import collections
 import copy
 import operator
 import subprocess
@@ -123,6 +122,11 @@ def _get_output_args(node, stream_name_map):
     fmt = kwargs.pop('format', None)
     if fmt:
         args += ['-f', fmt]
+    if 'video_size' in kwargs:
+        video_size = kwargs.pop('video_size')
+        if not isinstance(video_size, basestring) and isinstance(video_size, collections.Iterable):
+            video_size = '{}x{}'.format(video_size[0], video_size[1])
+        args += ['-video_size', video_size]
     args += _convert_kwargs_to_cmd_line_args(kwargs)
     args += [filename]
     return args
