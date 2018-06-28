122e7bc2e2df2db3afc0300712e6dbcfa3078105
Karl Kroening <karlk@kralnet.us>
2018-06-27 23:51:50 -0700

    Release 0.1.14

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from textwrap import dedent
 
-version = '0.1.13'
+version = '0.1.14'
 download_url = 'https://github.com/kkroening/ffmpeg-python/archive/v{}.zip'.format(version)
 
 long_description = dedent("""\
