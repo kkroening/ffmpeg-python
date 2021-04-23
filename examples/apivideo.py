c1623d03fb031b9bbdae95955aa4a22a1dcfb13f
Erikka <19295862+erikkai@users.noreply.github.com>
2021-04-22 23:13:49 -0700

    Added import statement

--- examples/apivideo.py
+++ examples/apivideo.py
@@ -2,12 +2,13 @@
 # Complete tutorial for using ffmpeg-python with api.video here: https://api.video/blog/tutorials/live-stream-to-the-browser-with-ffmpeg-cli-and-python
 # You need to get your api.video stream key and replace 'apivideo_stream_key.'
 # You can probably use this for other tools as well. 
+import ffmpeg
 
 rtmp_url = "rtmp://broadcast.api.video/s/" + apivideo_stream_key
 
 (
-ffmpeg
-.input('0:0', format='avfoundation', framerate=25)
-.output(rtmp_url, format='flv', flvflags='no_duration_filesize')
-.run()
+  ffmpeg
+  .input('0:0', format='avfoundation', framerate=25)
+  .output(rtmp_url, format='flv', flvflags='no_duration_filesize')
+  .run()
 )
